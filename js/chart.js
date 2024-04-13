fa1b8f9eaa3274b44bba3143defcd05604cbb44d
ioanaastefanoae <razvan.benchia@siemens.com>
2024-04-14 00:00:38 +0300

    added line chart

--- js/chart.js
+++ js/chart.js
@@ -2,7 +2,8 @@ import { loadHTMLFileIntoSection } from "../components/utils.js";
 
 document.addEventListener('DOMContentLoaded', function () {
     loadHTMLFileIntoSection('components/column-chart/column-chart.html', 'charts-container__column-chart', 'components/column-chart/column-chart.js');
-    loadHTMLFileIntoSection('components/pie-chart/pie-chart.html', 'charts-container__flex-container-pie-chart', 'components/column-chart/pie-chart.js');
+    loadHTMLFileIntoSection('components/pie-chart/pie-chart.html', 'charts-container__flex-container-pie-chart', 'components/pie-chart/pie-chart.js');
+    loadHTMLFileIntoSection('components/line-chart/line-chart.html', 'charts-container__flex-container-line-chart', 'components/line-chart/line-chart.js');
 
 });    
 
