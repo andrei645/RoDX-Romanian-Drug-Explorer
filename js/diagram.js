358d926be13ce8457cf4d27513b7493b485cd992
ioanaastefanoae <razvan.benchia@siemens.com>
2024-04-13 21:48:06 +0300

    - added map svg / functionality

--- js/diagram.js
+++ js/diagram.js
@@ -1,1 +1,67 @@
-console.log("Diagram script");
+console.log("entere")
+
+var objectElement = document.getElementById("myMap");
+  
+    objectElement?.addEventListener("load", function () {
+      var svgDoc = objectElement.contentDocument;
+  
+      if (svgDoc && svgDoc.getElementsByTagName("svg").length > 0) {
+  
+        var svgContainer = svgDoc.getElementsByTagName("svg")[0];
+  
+        var paths = svgContainer.querySelectorAll('path');
+        var counties = svgContainer.querySelectorAll('text');
+  
+        addMouseEventOnPaths(paths);
+        addMouseEventOnCounties(counties, svgContainer);
+  
+      }
+    });
+  
+  function addMouseEventOnCounties(counties, svgContainer) {
+    counties.forEach(function (county) {
+  
+      county.addEventListener('click', function(event){
+        var county = this.getAttribute('name');
+        window.location.href = "chart.html?country=" + encodeURIComponent(county);
+        
+      });
+      
+      county.addEventListener('mouseenter', function (event) {
+        var countyName = this.getAttribute('name');
+        
+        var correspondingPath = svgContainer.querySelector('path[name="' + countyName + '"]');
+  
+        if (correspondingPath) {
+          correspondingPath.setAttribute('fill', '#3da4d4');
+        }
+      });
+      county.addEventListener('mouseleave', function (event) {
+        var countyName = this.getAttribute('name');
+        var correspondingPath = svgContainer.querySelector('path[name="' + countyName + '"]');
+  
+        if (correspondingPath) {
+          correspondingPath.setAttribute('fill', '#8cd5f7');
+        }
+      });
+    });
+  }
+  function addMouseEventOnPaths(paths) {
+    paths.forEach(function (path) {
+      path.addEventListener('click', function(event){
+        var county = this.getAttribute('name');
+        window.location.href = "chart.html?county=" + encodeURIComponent(county);
+        
+      });
+      path.addEventListener('mouseenter', function (event) {
+        var countyName = this.getAttribute('name');
+  
+        this.setAttribute('fill', '#3da4d4');
+  
+      });
+      path.addEventListener('mouseleave', function (event) {
+        this.setAttribute('fill', '#8cd5f7');
+      });
+    });
+  }
+  
