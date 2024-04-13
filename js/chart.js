ca5191333c5109b2e876eac495d0d05ad8afe5a9
ioanaastefanoae <razvan.benchia@siemens.com>
2024-04-13 23:42:55 +0300

    add pie chart

--- js/chart.js
+++ js/chart.js
@@ -2,5 +2,7 @@ import { loadHTMLFileIntoSection } from "../components/utils.js";
 
 document.addEventListener('DOMContentLoaded', function () {
     loadHTMLFileIntoSection('components/column-chart/column-chart.html', 'charts-container__column-chart', 'components/column-chart/column-chart.js');
-    
-});
+    loadHTMLFileIntoSection('components/pie-chart/pie-chart.html', 'charts-container__flex-container-pie-chart', 'components/column-chart/pie-chart.js');
+
+});    
+
