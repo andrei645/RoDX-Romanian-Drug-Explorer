99c46664604452a17bfed464cda66ec5d65f71fa
ioanaastefanoae <razvan.benchia@siemens.com>
2024-04-13 23:37:01 +0300

    add column chart component

---
+++ js/chart.js
@@ -0,0 +1,6 @@
+import { loadHTMLFileIntoSection } from "../components/utils.js";
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadHTMLFileIntoSection('components/column-chart/column-chart.html', 'charts-container__column-chart', 'components/column-chart/column-chart.js');
+    
+});
