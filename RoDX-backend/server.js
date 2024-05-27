d7c09ee8ef2a0abf6d52fcf6de552847164a66c6
ioanaastefanoae <ioanaastefanoae0@gmail.com>
2024-05-27 20:17:00 +0300

    feat: connection with database

--- RoDX-backend/server.js
+++ RoDX-backend/server.js
@@ -1,12 +1,28 @@
 const http = require('http');
+const {Client } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env.development' });
+
+const pgConfig = {
+  user: process.env.USER || '',
+  host: process.env.HOST || '',
+  database: process.env.DATABASE || '',
+  password: process.env.PASSWORD || '',
+  port: process.env.PORT || '',
+};
+
+const client = new Client(pgConfig);
+
+client.connect()
+  .then(() => console.log('Connected.'))
+  .catch(err => console.error('Connection error', err.stack));
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (req.method === 'GET' && req.url === '/') {
-    res.end('Good job');
-  }
-    else {
+    res.end('Connected successfully!');
+  } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not found!');
   }
